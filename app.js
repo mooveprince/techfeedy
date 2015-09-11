cf9d7d770cee585a92219220470be35e51712421
mooveprince <mooveprince@gmail.com>
2015-09-10 22:57:13 -0500

    trendset

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var http = require('http');
 http.createServer(function(req, res) {
    var file = __dirname + req.url;
 
-   var query = require('url').parse(req.url,true).query;
-
    if(req.url === '/') {
        file = __dirname + '/index.html'   // serve index.html on root
-   } 
+   } else if(req.url === '/trends' || req.url.indexOf("lat") > -1  ) {  //if request is for trends & any QS is available
+      file = __dirname + '/trends.html';
+   }
 
    // serve all other files echoed by index.html e.g. style.css
    fileServer(file, req, res );    // callback is optional
