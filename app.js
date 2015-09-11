2bde8765b59ecffb41a10cdca85ade5d72d14357
mooveprince <mooveprince@gmail.com>
2015-09-10 21:17:06 -0500

    trends

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var fileServer = require('./fileserver');
 var http = require('http');
 http.createServer(function(req, res) {
    var file = __dirname + req.url;
+
+   var query = require('url').parse(req.url,true).query;
+
    if(req.url === '/') {
-       // serve index.html on root 
-       file = __dirname + '/index.html'
+       file = __dirname + '/index.html'   // serve index.html on root
+   } else if(req.url === '/trends' || typeof query.lat != undefined) {  //if request is for trends & any QS is available
+      file = __dirname + '/trends.html';
    }
+   
    // serve all other files echoed by index.html e.g. style.css
-   // callback is optional
-   fileServer(file, req, res );
+   fileServer(file, req, res );    // callback is optional
 
-}).listen(process.env.PORT || 8000);
-
+}).listen(process.env.PORT || 8008);
