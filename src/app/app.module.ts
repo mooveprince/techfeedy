4de5e025ed9cb79227ac5c4a21a3009e140e07bc
Moovendhan Elanchezhiyan <moove@Moovendhans-MacBook-Pro.local>
2021-08-10 16:19:10 -0400

    style change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SplashComponent } from './splash/splash.component';
